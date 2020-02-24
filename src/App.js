ed7b117d943449472610c6cbff523fdc7953be9a
Samuel Thng <samuel@thng.sg>
2020-02-24 19:59:14 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,214 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useCallback } from "react";
+import { css } from "emotion";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CardActionArea,
+  Slider,
+  Paper,
+  Typography,
+  CardActions,
+  Collapse
+} from "@material-ui/core";
 
-function App() {
+import { Close, RadioButtonUnchecked, Cancel } from "@material-ui/icons";
+
+import TicTacToe from "./Tic-Tac-Toe";
+
+export default function App() {
+  const [size, setSize] = useState(3);
+  const handleSizeChanged = useCallback((event, value) => {
+    setSize(value);
+  }, []);
+
+  const styles = useMemo(
+    () => ({
+      container: css`
+        height: 100%;
+        width: 100%;
+        padding: 1em;
+        box-sizing: border-box;
+        position: relative;
+      `,
+      actionContainer: css`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      `,
+      sizeSliderContainer: css`
+        width: 5em;
+        margin-left: 1em;
+      `,
+      card: css`
+        height: 100%;
+        display: grid;
+        grid-template-rows: auto 1fr;
+      `,
+      board: css`
+        display: grid;
+        grid-template-columns: repeat(${size}, 1fr);
+        grid-template-rows: repeat(${size}, 1fr);
+        grid-gap: 0.5em;
+      `,
+      positionButton: css`
+        height: 100%;
+      `,
+      positionContainer: css`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      `
+    }),
+    [size]
+  );
+
+  const [game, setGame] = useState(null);
+  const [board, setBoard] = useState([]);
+  const [player, setPlayer] = useState(null);
+  const [mark, setMark] = useState(null);
+
+  const onBoardChanged = useCallback(nextBoard => {
+    setBoard(nextBoard);
+  }, []);
+
+  const onPlayerChanged = useCallback((p, m) => {
+    setPlayer(p);
+    setMark(m);
+  }, []);
+
+  const onGameEnded = useCallback((gameEnded, winner, mark) => {
+    if (gameEnded) {
+      console.log({ gameEnded, winner, mark });
+      setGame(null);
+      if (winner !== null) {
+        setPlayer(winner);
+        setMark(mark);
+      } else {
+        setPlayer(false);
+        setMark(false);
+      }
+    }
+  }, []);
+
+  const startGameHandler = useCallback(() => {
+    const newGame = new TicTacToe(
+      <Close color="primary" />,
+      <RadioButtonUnchecked color="secondary" />,
+      size
+    );
+    setGame(newGame);
+
+    const unregisterBoard = newGame.addListener(
+      "onBoardChanged",
+      onBoardChanged
+    );
+    const unregisterPlayer = newGame.addListener(
+      "onPlayerChanged",
+      onPlayerChanged
+    );
+
+    const unregisterGameEnded = newGame.addListener("onGameEnded", onGameEnded);
+
+    return () => {
+      setGame(null);
+
+      setBoard([]);
+      unregisterBoard();
+
+      setPlayer(null);
+      unregisterPlayer();
+
+      unregisterGameEnded();
+    };
+  }, [size, onBoardChanged, onPlayerChanged, onGameEnded]);
+
+  const title = useMemo(() => "Tic Tac Toe", []);
+
+  const gameState = useMemo(() => {
+    if (game === null && player === null) return null;
+    else if (game === null && player === false) return `Draw!`;
+    else if (game === null && player !== null) return "Winner!";
+    else return "Turn";
+  }, [game, player]);
+
+  const action = useMemo(() => {
+    if (game === null)
+      return (
+        <>
+          <div className={styles.sizeSliderContainer}>
+            <Slider
+              value={size}
+              min={3}
+              max={5}
+              onChange={handleSizeChanged}
+              valueLabelFormat={size => `${size}×${size}`}
+              valueLabelDisplay="auto"
+              marks
+            />
+          </div>
+          <Button variant="outlined" onClick={startGameHandler}>
+            {gameState === null ? "Play!" : "Play Again!"}
+          </Button>
+        </>
+      );
+    else return null;
+  }, [
+    game,
+    startGameHandler,
+    handleSizeChanged,
+    size,
+    styles.sizeSliderContainer
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Card className={styles.card}>
+        <CardHeader
+          component={Paper}
+          title={title}
+          square
+          action={
+            <>
+              <Typography variant="caption">{gameState}</Typography>
+              <div>
+                {gameState === "Draw!" ? <Cancel color="disabled" /> : mark}
+              </div>
+            </>
+          }
+        />
+        <CardContent className={styles.board}>
+          {board.map((s, p) => {
+            return (
+              <Card key={p}>
+                <CardActionArea
+                  onClick={
+                    s === null && game !== null
+                      ? () => game.selectPosition(p)
+                      : undefined
+                  }
+                  disabled={s !== null || game === null}
+                  className={styles.positionButton}
+                >
+                  <CardContent className={styles.positionContainer}>
+                    {s}
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            );
+          })}
+        </CardContent>
+        <Collapse in={action !== null}>
+          <Paper
+            square
+            component={CardActions}
+            className={styles.actionContainer}
+          >
+            {action}
+          </Paper>
+        </Collapse>
+      </Card>
     </div>
   );
 }
-
-export default App;
---
+++ src/Tic-Tac-Toe/index.js
@@ -0,0 +1,328 @@
+class TicTacToe {
+  /** Rows of the board. */
+  _rows;
+
+  /** Columns of the board. */
+  _cols;
+
+  /** Size of the board. */
+  _size;
+
+  /** Board State */
+  _board;
+
+  /** Moves played */
+  _moves = 0;
+
+  /** X Marker */
+  _markerX;
+
+  /** O Marker */
+  _markerO;
+
+  /** Map of player identifier to marker. */
+  _playerMap;
+
+  _currentPlayer;
+
+  _gameEnded;
+
+  _winner = null;
+
+  constructor(X = "X", O = "O", size = 3) {
+    this._rows = size;
+    this._cols = size;
+    this._size = size;
+    this._board = Array(size * size).fill(null);
+
+    this._markerX = X;
+    this._markerO = O;
+    this._playerMap = {
+      0: this._markerX,
+      1: this._markerO
+    };
+
+    this._currentPlayer = Math.round(Math.random());
+  }
+
+  /**
+   * @returns Board state.
+   **/
+  get board() {
+    return this._board.map(s => this._playerMap[s] || null);
+  }
+
+  /**
+   * @returns Current player.
+   */
+  get currentPlayer() {
+    return this._currentPlayer;
+  }
+
+  get gameEnded() {
+    return this._gameEnded || false;
+  }
+
+  get winner() {
+    return this._winner;
+  }
+
+  checkWinningState(index) {
+    const wins = [];
+
+    // For any position, there will be horizontal or vertical win states.
+    const row = this.getRowOf(index);
+    wins.push(this.checkRowWin(row));
+
+    const col = this.getColOf(index);
+    wins.push(this.checkColWin(col));
+
+    const leftToRightDiagonal = row === col;
+    if (leftToRightDiagonal) {
+      wins.push(this.checkLeftToRightWin());
+    }
+
+    const rightToLeftDiagonal = this.flipRow(row) === col;
+    if (rightToLeftDiagonal) {
+      wins.push(this.checkRightToLeftWin());
+    }
+
+    return wins.some(win => win === true);
+  }
+
+  flipRow(row) {
+    return this._size - 1 - row;
+  }
+
+  getRowIndexes(row) {
+    return [...Array(this._size)].map((_, i) => this.getIndexOf(row, i));
+  }
+
+  getColIndexes(col) {
+    return [...Array(this._size)].map((_, i) => this.getIndexOf(i, col));
+  }
+
+  getLeftToRightIndexes() {
+    return [...Array(this._size)].map((_, i) => this.getIndexOf(i, i));
+  }
+
+  getRightToLeftIndexes() {
+    return [...Array(this._size)].map((_, i) =>
+      this.getIndexOf(this.flipRow(i), i)
+    );
+  }
+  checkRowWin(row) {
+    return this.checkWinningIndexes(this.getRowIndexes(row));
+  }
+
+  checkColWin(col) {
+    return this.checkWinningIndexes(this.getColIndexes(col));
+  }
+
+  checkLeftToRightWin() {
+    return this.checkWinningIndexes(this.getLeftToRightIndexes());
+  }
+
+  checkRightToLeftWin() {
+    return this.checkWinningIndexes(this.getRightToLeftIndexes());
+  }
+
+  checkWinningIndexes(indexes) {
+    return indexes.map(i => this._board[i]).every((s, i, a) => s === a[0]);
+  }
+
+  /** Makes a play on a position. */
+  selectPosition(index) {
+    this.checkValidIndex(index);
+
+    if (this._gameEnded) {
+      throw Error("Game has already ended.");
+    }
+
+    if (this._board[index] !== null) {
+      throw Error("Position has already been played.");
+    }
+
+    this._moves++;
+    this._board[index] = this.currentPlayer;
+    this.updateBoardListeners();
+
+    const hasWinner = this.checkWinningState(index);
+    if (hasWinner) {
+      this._winner = this.currentPlayer;
+      this._gameEnded = true;
+      this.updateGameEndedListeners();
+      console.log(
+        `Game has ended, the winner: Player ${this.currentPlayer}, "${
+          this._playerMap[this.currentPlayer]
+        }"`
+      );
+    } else if (this._moves === this._size * this._size) {
+      this._gameEnded = true;
+      this.updateGameEndedListeners();
+      console.log(`Game has ended with a draw!`);
+    } else {
+      this.togglePlayer();
+      console.log(`It is player: ${this._currentPlayer}'s turn.`);
+    }
+  }
+
+  /** Swap between players and fire an update. */
+  togglePlayer() {
+    this._currentPlayer = Number(!Boolean(this._currentPlayer));
+    this.updatePlayerListeners();
+  }
+
+  /** onBoardChanged Callbacks. */
+  onBoardChanged = [];
+
+  /** Fire callbacks for onBoardChanged. */
+  updateBoardListeners(callback) {
+    if (callback) callback(this.board);
+    else this.onBoardChanged.forEach(fn => fn(this.board));
+  }
+
+  /** onPlayerChanged Callbacks. */
+  onPlayerChanged = [];
+
+  /** Fire callbacks for onPlayerChanged. */
+  updatePlayerListeners(callback) {
+    if (callback)
+      callback(this.currentPlayer, this._playerMap[this.currentPlayer]);
+    else
+      this.onPlayerChanged.forEach(fn =>
+        fn(this.currentPlayer, this._playerMap[this.currentPlayer])
+      );
+  }
+
+  /** onGameEnded Callbacks. */
+  onGameEnded = [];
+
+  /** Fire callbacks for onGameEnded. */
+  updateGameEndedListeners(callback) {
+    if (callback)
+      callback(
+        this.gameEnded,
+        this.winner,
+        this._playerMap[this.winner] || null
+      );
+    else
+      this.onGameEnded.forEach(fn =>
+        fn(this.gameEnded, this.winner, this._playerMap[this.winner] || null)
+      );
+  }
+
+  /** Registers an event listener.
+   * @param event `onBoardChanged` | `onPlayerChanged` | `onGameEnded`
+   * @param callback Function to be called on a change event.
+   * @returns { function } Removes a callback function.
+   */
+  addListener(event, callback) {
+    if (typeof event !== "string") throw TypeError("Event must be a string.");
+    if (typeof callback !== "function")
+      throw TypeError("Callback must be a function.");
+
+    switch (event) {
+      case "onBoardChanged":
+        this.onBoardChanged.push(callback);
+        this.updateBoardListeners(callback);
+        return () => {
+          const index = this.onBoardChanged.findIndex(callback);
+          this.onBoardChanged.splice(index, 1);
+        };
+      case "onPlayerChanged":
+        this.onPlayerChanged.push(callback);
+        this.updatePlayerListeners(callback);
+        return () => {
+          const index = this.onPlayerChanged.findIndex(callback);
+          this.onPlayerChanged.splice(index, 1);
+        };
+      case "onGameEnded":
+        this.onGameEnded.push(callback);
+        this.updateGameEndedListeners(callback);
+        return () => {
+          const index = this.onGameEnded.findIndex(callback);
+          this.onPlayerChanged.splice(index, 1);
+        };
+      default:
+        return;
+    }
+  }
+
+  /** Validates the index of a board position.
+   * @param index Index of a board position.
+   * @throws { RangeError } Index is out of board range.
+   **/
+  checkValidIndex(index) {
+    if (index > this._board.length || index < 0) {
+      throw RangeError("Index is out of board range.");
+    }
+  }
+
+  /** Validates the row of a board.
+   * @param row Row of a board position.
+   * @throws { RangeError } Row is out of board range.
+   **/
+  checkValidRow(row) {
+    if (row < 0 || row > this._rows) {
+      throw RangeError("Row is out of board range.");
+    }
+  }
+
+  /** Validates the column of a board.
+   * @param col Column of a board position.
+   * @throws { RangeError } Column is out of board range.
+   **/
+  checkValidCol(col) {
+    if (col < 0 || col > this._cols) {
+      throw RangeError("Column is out of board range.");
+    }
+  }
+
+  /** Returns the index of a given row and column positions of a board.
+   * @param row Row of a board position.
+   * @param col Column of a board position.
+   * @returns { number } Index of a board position
+   **/
+  getIndexOf(row, col) {
+    this.checkValidRow(row);
+    this.checkValidCol(col);
+    return row * this._rows + col;
+  }
+
+  /** Returns the row of a given index position of a board.
+   * @param index Index of a board position.
+   * @returns { number } Row of a board position.
+   **/
+  getRowOf(index) {
+    this.checkValidIndex(index);
+    return Math.floor(index / this._size);
+  }
+
+  /** Returns the column of a given index position of a board.
+   * @param index Index of a board position.
+   * @returns { number } Column of a board position.
+   **/
+  getColOf(index) {
+    this.checkValidIndex(index);
+    return index % this._size;
+  }
+
+  /** Board utilities. */
+  get utilities() {
+    return {
+      validators: {
+        checkValidIndex: this.checkValidIndex.bind(this),
+        checkValidRow: this.checkValidRow.bind(this),
+        checkValidCol: this.checkValidCol.bind(this)
+      },
+      getters: {
+        getIndexOf: this.getIndexOf.bind(this),
+        getRowOf: this.getRowOf.bind(this),
+        getColOf: this.getColOf.bind(this)
+      }
+    };
+  }
+}
+
+export default TicTacToe;
